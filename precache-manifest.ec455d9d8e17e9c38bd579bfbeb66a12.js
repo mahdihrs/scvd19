848c2f12d53afb58a62df224e8f5f3cdf004db22
mahdihrs <mahdi.haris@myboost.id>
2020-03-27 15:03:31 +0700

    Updates

---
+++ precache-manifest.ec455d9d8e17e9c38bd579bfbeb66a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a88d47be2f9b4aee26ce70e2ad4424",
+    "url": "/scvd19/index.html"
+  },
+  {
+    "revision": "5321fac408d8d2493ad4",
+    "url": "/scvd19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d503cd19809084913c30",
+    "url": "/scvd19/static/js/2.a2864329.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/scvd19/static/js/2.a2864329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5321fac408d8d2493ad4",
+    "url": "/scvd19/static/js/main.3e3a67e2.chunk.js"
+  },
+  {
+    "revision": "911cec3e7e4f46305c40",
+    "url": "/scvd19/static/js/runtime-main.e1b5a7f5.js"
+  }
+]);
